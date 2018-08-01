f1aa96b4eb8c5e65c4a865c04e273ebe69a189e1
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2018-08-01 09:37:23 -0600

    Remove a bunch of stuff I no longer use

--- zsh/oh-my-zsh.zsh
+++
@@ -1,6 +0,0 @@
-export ZSH=/Users/tylerthrailkill/.oh-my-zsh
-# ZSH_THEME="edvardm"
-ZSH_THEME=""
-export NVM_LAZY_LOAD=true # speeds up startup
-plugins=(zsh-nvm brew bundler colorize common-aliases fancy-ctrl-z git git-flow git-prompt gitfast grunt gulp jsontools lol mvn nyan spring sublime thefuck themes fasd zsh-autosuggestions zsh-syntax-highlighting)
-source $ZSH/oh-my-zsh.sh
