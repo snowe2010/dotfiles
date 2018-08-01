7374b0ece346f3fed7f5020fa708163a38d73c6f
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2018-08-01 09:35:53 -0600

    Update env vars

--- system/env.zsh
+++ system/env.zsh
@@ -2,3 +2,12 @@ export EDITOR='vim'
 export ENCRYPTION_PW=1234
 export ENCRYPTION_SALT=1234
 export JWT_SIGNING_KEY=localKeyTellEverybody
+export LOCAL_TOKEN=true
+
+export JAVA_HOME=$(/usr/libexec/java_home)
+export M2_HOME=/usr/local/Cellar/maven/3.5.2/
+export GRADLE_HOME=~/Applications/ActiveVersions/Gradle
+export PHANTOM_JS=~/Applications/ActiveVersions/PhantomJS
+
+export PATH="$M2_HOME/bin:$GRADLE_HOME/bin:$PHANTOM_JS/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin" #:/usr/local/Cellar/elixir/1.5.1/bin"
+export MAVEN_OPTS="-XX:+TieredCompilation -XX:TieredStopAtLevel=1"
