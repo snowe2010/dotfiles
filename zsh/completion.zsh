344ecb2a24b2f451631d21ac416ab91c46bdb9d0
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2018-08-01 09:33:17 -0600

    Clean up a ton more completion stuff
    disable brew stuff by default

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,8 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# cache stuff for grunt at least
+zstyle ':completion:*' use-cache yes
+
+# zshaddhistory() { whence ${${(z)1}[1]} >| /dev/null || return 1 }
