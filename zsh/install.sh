d4eba8d740ecfbb9d244e4dbfe14d92d254f5012
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2018-10-31 17:01:34 -0600

    Update how shell gets set

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,5 +3,12 @@ if [ ! -d "$HOME/.zgen" ]; then
 fi
 # before this will succeed you need to add /usr/local/bin/zsh to /etc/shells :(
 if ! echo $SHELL | grep -q 'zsh'; then
-   chsh -s $(which zsh)
+    local shell_path;
+    shell_path="$(which zsh)"
+
+    if ! grep "$shell_path" /etc/shells > /dev/null 2>&1 ; then
+        echo "Adding '$shell_path' to /etc/shells"
+        sudo sh -c "echo $shell_path >> /etc/shells"
+    fi
+    chsh -s "$shell_path"
 fi
