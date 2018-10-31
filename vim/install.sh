0951fbe2bad849b99f3c2987975ea62580394a37
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2018-10-31 15:47:31 -0600

    Fix failure if vundle already installed

--- vim/install.sh
+++ vim/install.sh
@@ -1,2 +1,4 @@
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#vim +PluginInstall +qall
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    #vim +PluginInstall +qall
+fi
