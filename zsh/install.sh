3d98f2ab76725a0c34511c184e6603cf30e393b4
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2018-10-31 16:29:03 -0600

    Don't change login shell if it's currently zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,4 +2,6 @@ if [ ! -d "$HOME/.zgen" ]; then
     git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 fi
 # before this will succeed you need to add /usr/local/bin/zsh to /etc/shells :(
-chsh -s $(which zsh)
+if ! echo $SHELL | grep -q 'zsh'; then
+   chsh -s $(which zsh)
+fi
