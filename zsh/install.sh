cf90f6ba51afe76af39efb5e93b6966be3bfa28e
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2018-10-31 16:16:11 -0600

    Check for zgen first

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,5 @@
-git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+if [ ! -d "$HOME/.zgen" ]; then
+    git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+fi
 # before this will succeed you need to add /usr/local/bin/zsh to /etc/shells :(
 chsh -s $(which zsh)
