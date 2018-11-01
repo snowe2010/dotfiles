706e956362c0a7cb91ed09aaafd44ea41bd2449f
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2018-10-31 21:31:51 -0600

    Add visual studio code

--- karabiner/install.sh
+++ karabiner/install.sh
@@ -1,2 +1,3 @@
 mkdir -p ~/.config/karabiner/assets/complex_modifications
 cp **/*.json ~/.config/karabiner/assets/complex_modifications
+echo "You will need to enable this rule in Karabiner-Elements > Complex Modifications > Add rule"
