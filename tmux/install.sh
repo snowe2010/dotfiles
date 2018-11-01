3da4aae6a3d1e96a6150e7e6b3edc4117fef4940
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2018-11-01 11:57:34 -0600

    Add tpm to install

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,3 +1,7 @@
 if [ ! -d "$HOME/.tmuxifier" ]; then
     git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
 fi
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
