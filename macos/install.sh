8119eedeb4adc3a220871769bac1f838fd82640e
Tyler B. Thrailkill <tyler.b.thrailkill@gmail.com>
2020-12-07 12:26:28 -0700

    Use DOTFILES rather than DOTFILES_ROOT

--- macos/install.sh
+++ macos/install.sh
@@ -2,6 +2,7 @@
 # still be updates and installables in the Mac App Store. There's a nifty
 # command line interface to it that we can use to just install everything, so
 # yeah, let's do that.
+
 source $DOTFILES/bin/prompt.sh
 
 confirm_yes "Update Mac Software?" && {
