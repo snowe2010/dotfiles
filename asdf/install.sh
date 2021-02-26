a7160fbc53df3e02f0f3760d6b8d5c75b3436515
Tyler B. Thrailkill <tyler.b.thrailkill@gmail.com>
2021-02-26 10:53:10 -0700

    Add asdf install.sh

---
+++ asdf/install.sh
@@ -0,0 +1,13 @@
+asdf plugin install java
+asdf plugin install maven
+asdf plugin install nodejs
+asdf plugin install python
+asdf plugin install ruby
+asdf plugin install yarn
+
+asdf install java latest:adoptopenjdk-
+asdf install maven latest
+asdf install nodejs latest
+asdf install python latest
+asdf install ruby latest
+asdf install yarn latest
